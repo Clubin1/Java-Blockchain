2a7f027990a98bab3d674c4519b51b6d23eea785
Clubin1 <justin.tubay@gmail.com>
2022-10-17 02:54:02 -0400

    added more simple blockchain functionality

--- src/Block.java
+++ src/Block.java
@@ -7,7 +7,7 @@ public class Block {
     public String previousHash;
     private final String data;
     private final long timeStamp;
-
+    private int nonce;
     public Block(String data, String previousHash) {
         this.hash = calculateHash();
         this.data = data;
@@ -16,6 +16,19 @@ public class Block {
     }
 
     public String calculateHash() {
-        return StringUtil.applySha256(previousHash + Long.toString(timeStamp) + data);
+        return StringUtil.applySha256(
+                previousHash +
+                        Long.toString(timeStamp) +
+                        Integer.toString(nonce) +
+                        data
+        );
+    }
+    public void mineBlock(int difficulty) {
+        String target = new String(new char[difficulty]).replace('\0', '0');
+        while(!hash.substring(0, difficulty).equals(target)) {
+            nonce++;
+            hash = calculateHash();
+        }
+        System.out.println("Block Mined!!! : " + hash);
     }
 }
